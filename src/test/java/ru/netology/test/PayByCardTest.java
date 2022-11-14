228bcbb8c49234dbe38a6205f1d1047a4806c0f0
MaksimNosov <m.s.nosov@gmail.com>
2022-11-14 19:26:18 +0300

    Add PayByCardTest, ChoiceOfPaymentVariantPage, PaymentPage, DataHelper

---
+++ src/test/java/ru/netology/test/PayByCardTest.java
@@ -0,0 +1,295 @@
+package ru.netology.test;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.pages.ChoiceOfPaymentVariantPage;
+import ru.netology.pages.PaymentPage;
+//import ru.netology.data.SQLHelper;
+import ru.netology.pages.PaymentPage;
+
+import static com.codeborne.selenide.Selenide.*;
+//import static ru.netology.data.SQLHelper.cleanAuthCodesInDatabase;
+//import static ru.netology.data.SQLHelper.cleanDatabase;
+
+public class LoginTest {
+
+//    @SneakyThrows
+//    @Test
+//    void fff() {
+//        var choiceOfPaymentVariantPage = open("http://localhost:8080/", ChoiceOfPaymentVariantPage.class);
+//        choiceOfPaymentVariantPage.payByCard();
+//    }
+
+    @Test
+    void successfulPayment() {
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        make.makeSuccessfulPayment(DataHelper.getValidInfoForPayByCard());
+    }
+
+    @Test
+    void declinedPayment() { //падает, сделать issue
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getDeclinedCardNumber(),
+                DataHelper.getCurrentMonth(), DataHelper.getCurrentYear(), DataHelper.getValidCardOwner(),
+                DataHelper.getRandomCvcCode());
+        make.makeDeclinedPayment(info);
+    }
+
+    @Test
+    void randomCardNumberValidFormat() {
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getRandomCardNumberValidFormat(),
+                DataHelper.getCurrentMonth(), DataHelper.getCurrentYear(), DataHelper.getValidCardOwner(),
+                DataHelper.getRandomCvcCode());
+        make.makeDeclinedPayment(info);
+    }
+
+    @Test
+    void randomCardNumberInvalidFormat() {
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getRandomCardNumberInvalidFormat(),
+                DataHelper.getCurrentMonth(), DataHelper.getCurrentYear(), DataHelper.getValidCardOwner(),
+                DataHelper.getRandomCvcCode());
+        make.makeInvalidPayment(info);
+    }
+
+    @Test
+    void emptyCardNumber() {
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getEmptyCardNumber(),
+                DataHelper.getCurrentMonth(), DataHelper.getCurrentYear(), DataHelper.getValidCardOwner(),
+                DataHelper.getRandomCvcCode());
+        make.makeInvalidPayment(info);
+    }
+
+    @Test
+    void lastMonth() {
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        String year = DataHelper.getCurrentYear();
+        if (Integer.valueOf(DataHelper.getCurrentMonth()) == 1) {
+            year = String.valueOf((Integer.valueOf(DataHelper.getCurrentYear()) - 1));
+        }
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getApprovedCardNumber(),
+                DataHelper.getPastMonth(), year, DataHelper.getValidCardOwner(), DataHelper.getRandomCvcCode());
+        make.makePaymentInvalidMonth(info);
+    }
+
+    @Test
+    void monthPlus12() {
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        String invalidMonth = String.valueOf(Integer.valueOf(DataHelper.getCurrentMonth()) + 12);
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getApprovedCardNumber(),
+                invalidMonth, DataHelper.getCurrentYear(), DataHelper.getValidCardOwner(), DataHelper.getRandomCvcCode());
+        make.makePaymentInvalidMonth(info);
+    }
+
+    @Test
+    void emptyMonth() {
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getApprovedCardNumber(),
+                DataHelper.getEmptyMonth(), DataHelper.getCurrentYear(), DataHelper.getValidCardOwner(),
+                DataHelper.getRandomCvcCode());
+        make.makePaymentEmptyMonth(info);
+    }
+
+    @Test
+    void pastYear() {
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getApprovedCardNumber(),
+                DataHelper.getCurrentMonth(), DataHelper.getPastYear(), DataHelper.getValidCardOwner(),
+                DataHelper.getRandomCvcCode());
+        make.makePaymentInvalidYear(info);
+    }
+    @Test
+    void emptyYear() {
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getApprovedCardNumber(),
+                DataHelper.getCurrentMonth(), DataHelper.getEmptyYear(), DataHelper.getValidCardOwner(),
+                DataHelper.getRandomCvcCode());
+        make.makePaymentEmptyYear(info);
+    }
+
+    @Test
+    void cardOwnerPlusSpecSymbol() { //падает, сделать issue
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getApprovedCardNumber(),
+                DataHelper.getCurrentMonth(), DataHelper.getCurrentYear(), DataHelper.getCardOwnerSpecSymbol(),
+                DataHelper.getRandomCvcCode());
+        make.makePaymentInvalidCardOwner(info);
+    }
+
+    @Test
+    void cardOwnerPlusNumber() { //падает, сделать issue
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getApprovedCardNumber(),
+                DataHelper.getCurrentMonth(), DataHelper.getCurrentYear(), DataHelper.getInvalidCardOwnerByNumber(),
+                DataHelper.getRandomCvcCode());
+        make.makePaymentInvalidCardOwner(info);
+    }
+
+    @Test
+    void cardOwnerByRus() { //падает, сделать issue
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getApprovedCardNumber(),
+                DataHelper.getCurrentMonth(), DataHelper.getCurrentYear(), DataHelper.getInvalidCardOwnerByRus(),
+                DataHelper.getRandomCvcCode());
+        make.makePaymentInvalidCardOwner(info);
+    }
+
+    @Test
+    void emptyCardOwner() {
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getApprovedCardNumber(),
+                DataHelper.getCurrentMonth(), DataHelper.getCurrentYear(), DataHelper.getEmptyCardOwner(),
+                DataHelper.getRandomCvcCode());
+        make.makePaymentEmptyCardOwner(info);
+    }
+
+    @Test
+    void invalidCvcCode() {
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getApprovedCardNumber(),
+                DataHelper.getCurrentMonth(), DataHelper.getCurrentYear(), DataHelper.getValidCardOwner(),
+                DataHelper.getInvalidCvcCode());
+        make.makePaymentInvalidCvcCode(info);
+    }
+
+    @Test
+    void emptyCvcCode() { //падает, сделать issue
+        var choiceOfPaymentVariantPage = open("http://localhost:8080/",
+                ChoiceOfPaymentVariantPage.class);
+        choiceOfPaymentVariantPage.payByCard();
+        PaymentPage make = new PaymentPage();
+        DataHelper.InfoForPayByCard info = new DataHelper.InfoForPayByCard(DataHelper.getApprovedCardNumber(),
+                DataHelper.getCurrentMonth(), DataHelper.getCurrentYear(), DataHelper.getValidCardOwner(),
+                DataHelper.getEmptyCvcCode());
+        make.makePaymentEmptyCvcCode(info);
+    }
+
+
+//    @AfterAll
+//    static void teardown() {
+//        cleanDatabase();
+//    }
+//
+//    @SneakyThrows
+//    @Test
+//    void shouldSuccessfulLogin() {
+//        cleanAuthCodesInDatabase();
+//        var loginPage = open("http://localhost:9999/", LoginPage.class);
+//        var authInfo = DataHelper.getValidAuthInfo();
+//        var verificationPage = loginPage.validLogin(authInfo);
+//        verificationPage.verifyVerificationPageVisibility();
+//        var verificationCode = SQLHelper.getVerificationCode();
+//        verificationPage.validVerify(verificationCode);
+//    }
+//
+//    @Test
+//    void shouldErrorMessageWhenInvalidUser() {
+//        var loginPage = open("http://localhost:9999/", LoginPage.class);
+//        var authInfo = DataHelper.generateRandomUser();
+//        loginPage.validLogin(authInfo);
+//        loginPage.verifyErrorNotificationVisibility();
+//    }
+//
+//    @Test
+//    void shouldErrorWhenInvalidVerificationCode() {
+//        var loginPage = open("http://localhost:9999/", LoginPage.class);
+//        var authInfo = DataHelper.getValidAuthInfo();
+//        var verificationPage = loginPage.validLogin(authInfo);
+//        verificationPage.verifyVerificationPageVisibility();
+//        var verificationCode = DataHelper.generateRandomVerificationCode();
+//        verificationPage.verify(verificationCode.getCode());
+//        verificationPage.verifyErrorNotificationVisibility();
+//    }
+//
+//    @Test
+//    void shouldErrorNotificationThenMore3EntryValidLoginAndInvalidPassword() { //issue
+//        var loginPage = open("http://localhost:9999/", LoginPage.class);
+//        var authInfo = DataHelper.getAuthInfoValidLoginInvalidPassword();
+//        loginPage.invalidLogin(authInfo);
+//        loginPage.invalidLogin(authInfo);
+//        loginPage.invalidLogin(authInfo);
+//        loginPage.errorNotificationWhenMore3EntryValidLoginAndInvalidPassword();
+//    }
+//
+//    @Test
+//    void shouldErrorNotificationWhenMore3EntryValidUser() {
+//        cleanAuthCodesInDatabase();
+//        var loginPage = open("http://localhost:9999/", LoginPage.class);
+//        var authInfo = DataHelper.getValidAuthInfo();
+//        var verificationPage = loginPage.validLogin(authInfo);
+//        verificationPage.verifyVerificationPageVisibility();
+//        var verificationCode = SQLHelper.getVerificationCode();
+//        verificationPage.validVerifyMore3Times(verificationCode);
+//
+//        var loginPage2 = open("http://localhost:9999/", LoginPage.class);
+//        var authInfo2 = DataHelper.getValidAuthInfo();
+//        var verificationPage2 = loginPage2.validLogin(authInfo2);
+//        verificationPage2.verifyVerificationPageVisibility();
+//        var verificationCode2 = SQLHelper.getVerificationCode();
+//        verificationPage.validVerifyMore3Times(verificationCode2);
+//
+//        var loginPage3 = open("http://localhost:9999/", LoginPage.class);
+//        var authInfo3 = DataHelper.getValidAuthInfo();
+//        var verificationPage3 = loginPage3.validLogin(authInfo3);
+//        verificationPage3.verifyVerificationPageVisibility();
+//        var verificationCode3 = SQLHelper.getVerificationCode();
+//        verificationPage.validVerifyMore3Times(verificationCode3);
+//
+//        var loginPage4 = open("http://localhost:9999/", LoginPage.class);
+//        var authInfo4 = DataHelper.getValidAuthInfo();
+//        var verificationPage4 = loginPage4.validLogin(authInfo4);
+//        verificationPage4.verifyVerificationPageVisibility();
+//        var verificationCode4 = SQLHelper.getVerificationCode();
+//        verificationPage.validVerifyMore3Times(verificationCode4);
+//        verificationPage.errorNotificationWhenMore3EntryValidUser();
+//    }
+}
